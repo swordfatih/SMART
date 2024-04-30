264c613171a7b77a1760086e1d374d1f6fc33ad4
Sarah MALARD <sarah.malard@insa-lyon.fr>
2024-04-30 12:00:29 +0200

    feat: unity connexion serveur fonctionnelle

---
+++ apps/graphical/Assets/Code/RequestType.cs
@@ -0,0 +1,14 @@
+namespace Network
+{
+    public enum RequestType
+    {
+        Error,
+        Connect,
+        Start,
+        Message,
+        Choice,
+        Input,
+        NotifyPlayer,
+        NotifyBoard
+    }
+}
