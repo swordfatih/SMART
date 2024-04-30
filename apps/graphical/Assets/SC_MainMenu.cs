ed7afd365a8dfce6c8890b11d73781e4b31526b9
dtchoffonz <dilandtchoffo@gmail.com>
2024-04-30 11:08:02 +0200

    feat : main menu changes

--- apps/graphical/Assets/SC_MainMenu.cs
+++ apps/graphical/Assets/SC_MainMenu.cs
@@ -4,23 +4,25 @@ using UnityEngine;
 
 public class SC_MainMenu : MonoBehaviour
 {
+    public GameObject inputField;
     public GameObject mainMenu;
     public GameObject joinMenu;
     public GameObject startGameMenu;
-    private GameManager gameManager;
+    //private GameManager gameManager;
 
 
     // Start is called before the first frame update
     void Start()
     {
         MainMenuButton();
-        gameManager = FindObjectOfType<GameManager>();
+        //gameManager = FindObjectOfType<GameManager>();
     }
 
-    public void LaunchButton()
+    public void StartGameMenuButton()
     {
         // Play Now Button has been pressed, here you can initialize your game (For example Load a Scene called GameLevel etc.)
         //UnityEngine.SceneManagement.SceneManager.LoadScene("startScene");
+        inputField.SetActive(true);
         mainMenu.SetActive(false);
         joinMenu.SetActive(false);
         startGameMenu.SetActive(true);
@@ -29,19 +31,21 @@ public class SC_MainMenu : MonoBehaviour
     public void MainMenuButton()
     {
         // Show Main Menu
+        inputField.SetActive(false);
         mainMenu.SetActive(true);
         joinMenu.SetActive(false);
         startGameMenu.SetActive(false);
     }
 
-    public void JoinButton()
+    public void JoinMenuButton()
     {
+        inputField.SetActive(true);
         mainMenu.SetActive(false);
         joinMenu.SetActive(true);
         startGameMenu.SetActive(false);
-        Debug.Log("Bouton cliqué!");
-        gameManager.globalScore += 1;
-        Debug.Log(gameManager.globalScore);
+        //Debug.Log("Bouton cliquï¿½!");
+        //gameManager.globalScore += 1;
+        //Debug.Log(gameManager.globalScore);
 
     }
 
