4c8b5823a63f10e098c06f578c5d82518e4d3908
Sarah MALARD <sarah.malard@insa-lyon.fr>
2024-04-30 08:34:12 +0200

    Shared variables example (gameManager)

---
+++ apps/graphical/Assets/GameManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int globalScore = 0; //variable partagée
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
