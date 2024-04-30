97b1c5c10cc09aa95b8a5458af3d083d88eeafa6
Sarah MALARD <sarah.malard@insa-lyon.fr>
2024-04-30 11:50:20 +0200

    feat: debut reseau unity

---
+++ apps/graphical/Assets/Code/Client.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Net.Sockets;
+using Network;
+
+namespace Interface
+{
+    public class Client
+    {
+        public string Name { get; }
+        public Node Node { get; }
+
+        public Client(Node node, string name)
+        {
+            Name = name;
+            Node = node;
+
+            node.Send(RequestType.Connect, name);
+        }
+
+        public void Handle()
+        {
+            var packets = Node.Receive();
+
+            foreach (var packet in packets)
+            {
+                if (packet.Request == RequestType.Message)
+                {
+                    Console.WriteLine(packet.Content[0]);
+                }
+                else if (packet.Request == RequestType.Input)
+                {
+                    Console.WriteLine(packet.Content[0]);
+
+                    var input = Console.ReadLine() ?? "";
+                    Node.Send(RequestType.Message, input);
+                }
+                else if (packet.Request == RequestType.Choice)
+                {
+                    Console.WriteLine(packet.Content[0]);
+
+                    var input = Console.ReadLine() ?? "";
+                    Node.Send(RequestType.Choice, input);
+                }
+            }
+        }
+    }
+}
--- apps/graphical/Assets/Code/GameManager.cs
+++ apps/graphical/Assets/Code/GameManager.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Interface;
+using Network;
 
 public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
     public int globalScore = 0; //variable partagée
+    public Client client;
+
 
     void Start()
     {
