4bc689b216c6cea203d1e82dac0c191bf816f1d9
swordfatih <accfldekur@gmail.com>
2024-05-04 10:40:23 +0200

    feat: soap

--- apps/game/src/State/ShowerState.cs
+++ apps/game/src/State/ShowerState.cs
@@ -6,6 +6,11 @@ namespace Game
     {
         public override Action Action(Board board, Player player)
         {
+            if (player.Items.Any(x => x is Soap))
+            {
+                return new UseSoapAction(player);
+            }
+
             var targets = board.Players.Except(Status.Dead).Except(player);
             return new VoteAction(player, targets.ElementAt(player.Client.AskChoice(new("Contre qui vous voulez voter ?", new(targets.Select(x => x.Client.Name))))));
         }
